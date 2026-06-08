f44a5971d5f1ba6bfeb1559620f1e6cdc852e7df
Nangil <surasit.dang@bumail.net>
2026-06-08 23:16:27 +0700

    Add project files.

---
+++ HelpdeskSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelpdeskSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HelpdeskSystem/Tickets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace HelpdeskSystem
+{
+    public class Tickets
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ticketsId { get; set; }
+
+        public string title { get; set; }
+
+        public string priority { get; set; }
+
+        public string status { get; set; }
+    }
+}
